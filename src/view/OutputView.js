c6c64d903a71621ab9831cf006962dadbb190fea
kim-young-dong <opa9904@gmail.com>
2023-11-13 15:04:44 +0900

    feat: UI 로직 추가
    - InputView: 사용자로 부터 받은 입력값을 반환
    - OutputView: 필요한 인자를 받아 각종 메시지를 출력

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,11 +1,51 @@
 import { Console } from '@woowacourse/mission-utils';
 
 const OutputView = {
-  printMenu() {
-    Console.print('<주문 메뉴>');
-    // ...
+  printWellcome() {
+    Console.print('안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.');
   },
-  // ...
+  printPreview(date) {
+    Console.print(`12월 ${date}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!`);
+  },
+  printMenu(menus) {
+    Console.print('\n<주문 메뉴>');
+    menus.forEach(menu => {
+      Console.print(`${menu.name} - ${menu.count}개`);
+    });
+  },
+  printOrderPriceBeforeDiscount(price) {
+    Console.print('\n<할인 전 총주문 금액>');
+    Console.print(`${price}원`);
+  },
+  printGiftMenu(giftPresented) {
+    Console.print('\n<증정 메뉴>');
+    Console.print(`${giftPresented ? '샴페인 1개' : '없음'}`);
+  },
+  printDetailedBenefit(benefits) {
+    Console.print('\n<혜택 내역>');
+    if (benefits.length === 0) {
+      Console.print('없음');
+    } else {
+      benefits.forEach(benefit => {
+        Console.print(`${benefit.name}: -${benefit.price.toLocaleString()}원`);
+      });
+    }
+  },
+  printTotalBenefitPrice(price) {
+    Console.print('\n<총혜택 금액>');
+    Console.print(`-${price.toLocaleString()}원`);
+  },
+  printOrderPriceAfterDiscount(price) {
+    Console.print('\n<할인 후 예상 결제 금액>');
+    Console.print(`${price.toLocaleString()}원`);
+  },
+  printEventBadge(badge) {
+    Console.print('\n<12월 이벤트 배지>');
+    Console.print(`${badge}`);
+  },
+  printError(error) {
+    Console.print(error.message);
+  },
 };
 
 export default OutputView;
