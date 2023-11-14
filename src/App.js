dea4b3f684d16cb362490e3a67e07f816a3fff04
kim-young-dong <opa9904@gmail.com>
2023-11-14 11:41:12 +0900

    feat: 예약날짜, 주문 입력 및 검증기능 App.js에 연결
    - 이용자로부터 예약날짜와 주문을 입력받고 해당 값을 검증
    - 검증 완료시 OutputView를 통해 주문 메뉴와 총주문 금액 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,54 @@
+import InputView from './view/InputView';
+import OutputView from './view/OutputView';
+import DateValidator from './validator/DateValidator';
+import OrderValidator from './validator/OrderValidator';
+import MENU from './constant/Menu';
+
 class App {
-  async run() {}
+  #date;
+  #menus;
+  #totalOrderPrice;
+  #benefits;
+
+  async #inputDate() {
+    try {
+      const date = await InputView.readDate();
+      DateValidator.validate(date);
+      return Number(date);
+    } catch (error) {
+      OutputView.printError(error.message);
+      return this.#inputDate();
+    }
+  }
+
+  async #inputOrder() {
+    try {
+      let order = await InputView.readOrder();
+      order = OrderValidator.validate(order);
+      return order;
+    } catch (error) {
+      OutputView.printError(error.message);
+      return this.#inputOrder();
+    }
+  }
+
+  #calculateOrderPriceBeforeDiscount() {
+    const orderedMenus = MENU.filter(menu => this.#menus.find(({ name }) => name === menu.name));
+    const totalOrderPrice = orderedMenus.reduce(
+      (acc, cur) => acc + cur.price * this.#menus.find(({ name }) => name === cur.name).count,
+      0
+    );
+    return totalOrderPrice;
+  }
+
+  
+  async run() {
+    this.#date = await this.#inputDate();
+    this.#menus = await this.#inputOrder();
+    OutputView.printMenu(this.#menus);
+    this.#totalOrderPrice = this.#calculateOrderPriceBeforeDiscount();
+    OutputView.printOrderPriceBeforeDiscount(this.#totalOrderPrice);
+  }
 }
 
 export default App;
