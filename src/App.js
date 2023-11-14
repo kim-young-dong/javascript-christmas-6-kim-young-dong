7918d4c83466bea41b8aadf8914db155a4ab3228
kim-young-dong <opa9904@gmail.com>
2023-11-14 18:19:42 +0900

    feat: App.js에 혜택 계산기 연결

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import InputView from './view/InputView';
 import OutputView from './view/OutputView';
 import DateValidator from './validator/DateValidator';
 import OrderValidator from './validator/OrderValidator';
+import BenefitCalculator from './domain/BenefitCalculator';
 import MENU from './constant/Menu';
 
 class App {
@@ -41,13 +42,50 @@ class App {
     return totalOrderPrice;
   }
 
-  
+  #calculateBenefits() {
+    const calculator = new BenefitCalculator(this.#date, this.#menus, this.#totalOrderPrice);
+    this.#benefits = calculator.run();
+  }
+
+  #outPutBenefits() {
+    const hasGiftEvent = this.#benefits.find(({ name }) => name === '증정 이벤트');
+    OutputView.printGiftMenu(!!hasGiftEvent);
+    OutputView.printDetailedBenefit(this.#benefits);
+  }
+
+  #calculteTotalBenefitPrice() {
+    return this.#benefits.reduce((acc, { price }) => acc + price, 0);
+  }
+
+  #calculateOrderPriceAfterDiscount() {
+    return this.#calculateOrderPriceBeforeDiscount() - this.#calculteTotalBenefitPrice();
+  }
+
+  #calculateEventBadge() {
+    const totalBenefitPrice = this.#calculteTotalBenefitPrice();
+    if (totalBenefitPrice >= 20000) {
+      return '산타';
+    }
+    if (totalBenefitPrice >= 10000) {
+      return '트리';
+    }
+    if (totalBenefitPrice >= 5000) {
+      return '별';
+    }
+    return '없음';
+  }
+
   async run() {
     this.#date = await this.#inputDate();
     this.#menus = await this.#inputOrder();
     OutputView.printMenu(this.#menus);
     this.#totalOrderPrice = this.#calculateOrderPriceBeforeDiscount();
     OutputView.printOrderPriceBeforeDiscount(this.#totalOrderPrice);
+    this.#calculateBenefits();
+    this.#outPutBenefits();
+    OutputView.printTotalBenefitPrice(this.#calculteTotalBenefitPrice());
+    OutputView.printOrderPriceAfterDiscount(this.#calculateOrderPriceAfterDiscount());
+    OutputView.printEventBadge(this.#calculateEventBadge());
   }
 }
 
